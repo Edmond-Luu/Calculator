df431e47d216bd441d18980485b25edb3fe629d3
Edmond Luu <eluu472@gmail.com>
2022-06-09 18:23:42 -0700

    Calculator

---
+++ app.js
@@ -0,0 +1,35 @@
+let display = document.querySelector('#display');
+
+let buttons = Array.from(document.querySelectorAll(".button"));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = "";
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText)
+                }
+                catch (e) {
+                    display.innerText = "Error!"
+                    break;
+                }
+                break;
+            default:
+                if (display.innerText === "Error!") {
+                    display.innerText = "";
+                }
+                if (display.innerText.length < 18) {
+                    display.innerText += e.target.innerText;
+                }
+        }
+
+    })
+})
